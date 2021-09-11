d45c95a7c03ecc3f4ae0d8897ba7a6eaa3e974c4
aman shrivastava <aman34503@gmail.com>
2021-09-11 21:26:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+
+    // BEM
+    <div className="app">
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </ Router>
+    </div>
+  );
+}
+
+export default App;
