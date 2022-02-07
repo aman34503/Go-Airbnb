86e4fe720f8ca72d83468cdba8f6fc22c587eb8b
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-02-07 12:04:01 +0530

    redirect to homepage feature added

--- src/Header.js
+++ src/Header.js
@@ -10,12 +10,13 @@ import "./Header.css"
 function Header() {
 	return (
 		<div className="header">
+			<Link className="link" to="/">
 			<img
 				className="header__icon"
 				src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
 				alt=""
 			/>
-
+			</Link>
 			<div className="header__center">
 				<input type="text" placeholder="Start your search" />
 				<Button>
