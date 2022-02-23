b04bfe98a17bac6606d8d5d0efc6d776bb091135
Srijita Mandal <srijitamandal6798@gmail.com>
2022-02-23 11:23:31 +0530

    Added about us section

--- src/Card.js
+++ src/Card.js
@@ -3,14 +3,17 @@ import './Card.css'
 
 function Card({ src, title, description, price }) {
     return (
+        <>
         <div className='card'>
             <img src={src} alt="" />
-            <div className="card__info">
+            {/* <div className="card__info">
                 <h2>{title}</h2>
                 <h4>{description}</h4>
                 <h3>{price}</h3>
-            </div>
+            </div> */}
         </div>
+           
+        </>
     )
 }
 
