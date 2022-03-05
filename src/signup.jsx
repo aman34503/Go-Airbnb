6ece5bc598762c6d872742f22008ceaa6ed0b0a1
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-03-05 11:57:22 +0530

    signup

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
-import "./login.css"
-import login from "./login.svg"
+import "./signup.css"
+import sign from "./sign.svg"
 import { Link } from 'react-router-dom'
 
-export default function Login() {
+export default function Signup() {
   return (
       <div className='wrap'>
     <div className="containerlogin" id="container">
 		<div className="form-container log-in-container">
 			<form className='loginform' action="#">
-				<h1 className='hr1'>Login</h1>
+				<h1 className='hr1'>SignUp</h1>
 				<div className="social-container">
 					<a href="#" className="social"><i className="fa fa-facebook fa-2x"></i></a>
 					<a href="#" className="social"><i className="fab fa fa-twitter fa-2x"></i></a>
 					<a href="#" className="social"><i className="fab fa fa-instagram fa-2x"></i></a>
 					<a href="#" className="social"><i className="fab fa fa-google fa-2x"></i></a>
 				</div>
-				<span className='loginspan'>or use your account</span>
+				<span className='loginspan'>or Enter your Details</span>
 				<input className='logininput' type="email" placeholder="Email" />
-				<input className='logininput' type="password" placeholder="Password" />
-				<a href="#" className='fr'>Forgot your password?</a>
-				<Link to='/home'>
-				<button className='loginbutton'>Log In</button>
-				</Link>
-				<h5 className='hr1'> Don't have account? </h5>
-				<Link to='/Signup' >
-					<button className='p1'>Please Sign In</button> 
+				<input className='logininput' type="text" placeholder="Enter your name" />
+				<input className='logininput' type="tel" placeholder="Enter Your phone number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+				<input className='logininput' type="password" placeholder="Enter Your Password" />
+				<input className='logininput' type="password" placeholder="Confirm Password" />
+				<Link to='/'>
+				<button className='loginbutton'>Sign Up</button>
 				</Link>
 			</form>
 		</div>
 		<div className="overlay-container">
 			<div className="overlay">
 				<div className="overlay-panel overlay-right">
-					<img className='signinimg' src={login} alt="logo" />
+					{/* <h1 className='hr1'>Don't have account?</h1>
+					<p className='p1'>Please Sign In</p> yo*/}
+					<img className='signinimg' src={sign} alt="logo" />
 				</div>
 			</div>
 		</div>
